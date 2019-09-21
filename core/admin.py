47d081befdb5ec4fecba75a24b972cd3be8306e3
Rafizan Baharum <rafizan.baharum@canang.com.my>
2019-09-21 13:20:07 +0800

    Add models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from core.models import Volunteer, Staff, User
+from core.models import Volunteer, Staff, User, State, City, Level
 
 admin.site.register(User)
 admin.site.register(Staff)
 admin.site.register(Volunteer)
+admin.site.register(Level)
+admin.site.register(City)
+admin.site.register(State)
