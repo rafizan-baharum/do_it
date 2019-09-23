5426dcdd63305348cb371b3d5d58fabd2765cf66
Rafizan Baharum <rafizan.baharum@canang.com.my>
2019-09-23 21:28:25 +0800

    Fix for admin

--- do_it/middlewares.py
+++ do_it/middlewares.py
@@ -9,17 +9,11 @@ from core.models import Doer, Staff
 
 logger = logging.getLogger(__name__)
 
-
 class DoerWalletMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        # One-time configuration and initialization.
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-
-        # count message and notification
         if request.user.is_authenticated:
             if (request.user.is_doer):
                 earning = DoerWallet.objects.filter(doer_id=get_doer(request)) \
@@ -32,23 +26,14 @@ class DoerWalletMiddleware:
 
                 request.session['sum_wallet'] = str(sumPoint)
         response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
         return response
 
 
 class CurrentUserMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        # One-time configuration and initialization.
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-
-        # count message and notification
         if request.user.is_authenticated:
             if request.user.is_superuser:
                 pass
@@ -60,10 +45,6 @@ class CurrentUserMiddleware:
                 pass
 
         response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
         return response
 
 
